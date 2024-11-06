d264a271d6be659d997bd41bf3fa8ab515d6e637
kmyas <kmyas@YASHAWANTH>
2024-11-06 14:38:02 +0530

    changes in pom page

--- ComcastCRMGUIFramework3/src/main/java/com/comcast/crm/objectRepositoryutility/ProductsPage.java
+++ ComcastCRMGUIFramework3/src/main/java/com/comcast/crm/objectRepositoryutility/ProductsPage.java
@@ -19,6 +19,7 @@ public class ProductsPage {
 	}
 	@FindBy(linkText = "Productsdropdown")
 	private WebElement ProductsddLink;
+	
 	public WebElement getProductsddLink()
 	{
 		return ProductsddLink;

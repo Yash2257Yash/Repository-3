c66761ac04b2e01685d3602f657db314dbb4893c
kmyas <kmyas@192.168.137.230>
2024-11-06 12:33:05 +0530

    product page created

---
+++ ComcastCRMGUIFramework3/src/main/java/com/comcast/crm/objectRepositoryutility/ProductsPage.java
@@ -0,0 +1,21 @@
+package com.comcast.crm.objectRepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductsPage {
+	WebDriver driver;
+	public ProductsPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+	@FindBy(linkText = "Products")
+	private WebElement ProductsLink;
+	public WebElement getProductsLink()
+	{
+		return ProductsLink;
+	}
+
+}
